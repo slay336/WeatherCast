8a9604470b9f0382578ccc5d498643c8303d3377
Slavik Uvarov <vyacheslav.uvarov@gmail.com>
2020-11-11 20:47:30 +0300

    Фавикон, изменение верстки

--- visual/static/js/script.js
+++ visual/static/js/script.js
@@ -1,6 +1,16 @@
+Vue.component('currentimage', {
+    props: ['imagename'],
+    template: '<img :src="\'static/img/\' + imagename + \'.png\'">'
+})
+
 var app = new Vue({
-    el: '#app',
+    el: '#page',
     data: {
-        info: 'Тестовая инфа'
+        currentDate: 'Monday, may 23',
+        currentTemperature: '27 C / 81F',
+        currentTime: '14:35',
+        realFeel: 'Real Feel 25 C / 76F',
+        humidity: 'Humidity 61%',
+        currentImage: '01d'
     }
 })
